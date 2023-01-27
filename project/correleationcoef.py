f17fa71dff4a9aa39bf32645e29d8c62f37dfb1d
Subhra Pattnaik <50340351+subhrapattnaik@users.noreply.github.com>
2023-01-27 18:22:06 +0530

    Create correleationcoef.py

---
+++ project/correleationcoef.py
@@ -0,0 +1,18 @@
+import plotly.express as px
+import csv
+import numpy as np
+
+
+temperature=[]
+ice_cream_sales=[]
+    
+with open("./temperatureicecream.csv") as csv_file:
+  csv_reader=csv.DictReader(csv_file)
+  
+  for row in csv_reader:
+     temperature.append(float(row["Temperature"]))
+     ice_cream_sales.append(float(row["Ice-cream-Sales"]))
+
+correlation=np.corrcoef(temperature,ice_cream_sales)
+print("Correlation between Temperature vs Ice cream Sales:-",correlation[0,1])
+
